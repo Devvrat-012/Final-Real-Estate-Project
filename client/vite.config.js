b805335abc6938bb30054dc686893ea638fcc762
DevStark <starkji555@gmail.com>
2024-06-07 23:50:21 +0530

    Minor update

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://final-real-estate-project.onrender.com',
+        target: 'https://final-real-estate-project.onrender.com/api',
         changeOrigin: true,
         secure: false,
       },
