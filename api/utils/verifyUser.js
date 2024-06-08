06d65343fb331a8a579c05af844870aeb7fc1b59
DevStark <starkji555@gmail.com>
2024-06-08 14:23:43 +0530

    Minor change

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,4 +1,4 @@
-import { errorHandller } from "./error.js";
+import { errorHandler } from "./error.js";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config();
@@ -6,16 +6,16 @@ dotenv.config();
 export const verifytToken = (req, res, next) => {
   const cookieString = req.headers.cookie;
   if (!cookieString) {
-    return next(errorHandller(401, "Unauthorized!"));
+    return next(errorHandler(401, "Unauthorized!"));
   }
   const accessToken = cookieString
     .split("; ")
     .find((row) => row.startsWith("access_token="))
     ?.split("=")[1];
-  if (!accessToken) return next(errorHandller(401, "Unauthorized!"));
+  if (!accessToken) return next(errorHandler(401, "Unauthorized!"));
 
   jwt.verify(accessToken, process.env.JWT_SECRET, (err, user) => {
-    if (err) return next(errorHandller(403, "Forbidden!"));
+    if (err) return next(errorHandler(403, "Forbidden!"));
     req.user = user.id;
 
     next();
