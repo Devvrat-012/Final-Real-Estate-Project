2fa3155836ac2624447467f7480a72d3b568177b
DevStark <starkji555@gmail.com>
2024-06-08 13:24:00 +0530

    Update in backend url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,9 +10,10 @@ import CreateListing from './pages/CreateListing';
 import UpdateListing from './pages/UpdateListing';
 import Listing from './pages/Listing';
 import Search from './pages/Search';
+import axios from 'axios';
 
 axios.defaults.withCredentials = true
-axios.defaults.baseURL = "https://final-real-estate-project.onrender.com/api/";
+axios.defaults.baseURL = "https://final-real-estate-project.onrender.com/";
 
 export default function App() {
   return (
